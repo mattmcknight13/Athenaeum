e73aafd42a561466b65036bf62751f2352f4fef7
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2020-10-20 11:19:35 -0500

    fixed auth issue for create on books, made it to where genre can be added later - help from david

--- db/migrate/20201019195211_add_genre_to_books.rb
+++ db/migrate/20201019195211_add_genre_to_books.rb
@@ -1,5 +1,5 @@
 class AddGenreToBooks < ActiveRecord::Migration[6.0]
   def change
-    add_reference :books, :genre, null: false, foreign_key: true
+    add_reference :books, :genre, foreign_key: true
   end
 end
