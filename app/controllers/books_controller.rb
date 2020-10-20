5398fbd512abe046002c31d2da8e8933110201e3
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2020-10-20 10:05:54 -0500

    tested add on users, while testing add on books got filter chain error working to fix auth

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,52 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  def show
+    render json: @book, include: :genre
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = @current_user
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def add_genre
+    @book = Book.find(params[:id])
+    @genre = Genre.find(params[:genre_id])
+    @book.genres << @genre
+    render json: @book, include: :genres
+  end
+
+  def destroy
+    @book.destroy
+  end
+  
+  private
+
+  def set_book
+   @book = Book.find(params[:id])
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :image)
+  end
 end
