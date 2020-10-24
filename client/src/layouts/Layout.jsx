c3c9cc8aca8adbceb0d90ea60899a6ac041d0b03
Matthew McKnight <matthewmcknight@matthews-mbp.home>
2020-10-24 18:23:25 -0500

    added footer, and playing around with header styling

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import './Layout.css'
 
 export default function Layout(props) {
@@ -11,6 +12,7 @@ export default function Layout(props) {
         handleLogout={handleLogout}
       />
       {props.children}
+      <Footer/>
     </div>
   )
 }
