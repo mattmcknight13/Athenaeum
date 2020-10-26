9479d404ae3c68fc8dea962f172f1190466449af
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2020-10-25 22:22:31 -0500

    linting and preparing for deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # # GET /users
-  # def index
-  #   @users = User.all
-
-  #   render json: @users
-  # end
-
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
 
   # POST /users
   def create
@@ -24,14 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
-
-
   private
-
-  # # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
 
   # Only allow a trusted parameter "allow list" through.
   def user_params
